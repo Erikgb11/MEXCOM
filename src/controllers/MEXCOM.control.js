1b558d90e2b827fc9d28706078e9763e52277f3c
FrankyRivers99 <frankrequena7812@outlook.com>
2023-06-14 02:48:22 -0600

    Especificaciones request HTML del frontend

--- src/controllers/MEXCOM.control.js
+++ src/controllers/MEXCOM.control.js
@@ -1,6 +1,40 @@
 import { getConnection } from "../database/database"
 
-//Controlador de prueba peticion de puntos de oferta 
+
+
+
+
+//Manejo de peticiones de acuerdo al orden de acceso se pueden presentar las siguientes peticiones para el backend
+
+/*  
+    Usuarios 
+        -Verificación de usuario (POST)
+        -Creación de usuario (POST)
+        -Despliegue informacion de usuario (GET)
+        -Modificacion de usuarios (PUT ó PATCH) 
+        -Creacion punto de oferta (POST)
+        -Despliegue informacion de punto de venta (GET)
+        -Insertar productos o servicios en punto de oferta (POST)
+        -Modificacion productos o servicios en punto de oferta (PUT ó PATCH)
+        -Despliegue informacion de productos o servicios (GET)
+        -Eliminacion de prodcutos (DELETE)
+        -Eliminacion de punto de oferta (DELETE)
+        -Eliminación de usuario (DELETE)
+
+        
+
+    Usuarios sin registrar
+        -Obtener los geoJSON de los grupos comerciales de la localidad elegida(GET)
+        -Obtener la información del grupo comercial seleccionado(nombre, numero ofertantes) (GET)
+        -Obtener los ofertantes por grupo comercial (GET)
+        -Obtener la información de un ofertante que se eligio (GET)
+        -Obtener la informacion de la calificacion y comentarios de ese ofertante (GET)
+        -Escribir un nuevo comentario (POST)
+        -Puntuar un comentario(PUT)
+*/
+
+
+
 //Le asignamos el tipo async a la funcion para determinar que la conexion es independiente del programa
 const getPuntosOferta= async(req, res) => {
     try {
